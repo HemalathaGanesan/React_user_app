bcb2abf88fe899bf4479ee591ad0261e768c11a2
hemalathaGanesan <hema.g.anandha@gmail.com>
2021-12-22 23:07:01 +0530

    sorting added

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,13 +1,39 @@
 import { Fragment } from "react";
-
+import { useNavigate, useLocation } from "react-router-dom";
 import QuoteItem from "./QuoteItem";
 import classes from "./QuoteList.module.css";
 
+const sortQuote = (quotes, asc) => {
+  debugger;
+  return quotes.sort((quoteA, quoteB) => {
+    if (asc) {
+      return quoteA.id > quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1;
+    }
+  });
+};
+
 const QuoteList = (props) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const queryparams = new URLSearchParams(location.search);
+  const isSortingAsc = queryparams.get("sort") === "asc";
+  const sortedQuotes = sortQuote(props.quotes, isSortingAsc);
+
+  console.log(sortedQuotes);
+  const changeSortingHandler = () => {
+    navigate("/quotes?sort=" + (isSortingAsc ? "desc" : "asc"));
+  };
   return (
     <Fragment>
+      <div>
+        <button onClick={changeSortingHandler}>
+          Sort {isSortingAsc ? "Descending" : "Ascending"}
+        </button>
+      </div>
       <ul className={classes.list}>
-        {props.quotes.map((quote) => (
+        {sortedQuotes.map((quote) => (
           <QuoteItem
             key={quote.id}
             id={quote.id}
--- src/pages/QuoteDetails.js
+++ src/pages/QuoteDetails.js
@@ -1,26 +1,36 @@
-import { Routes, Route, useParams } from "react-router-dom";
+import { Routes, Route, useParams, Link } from "react-router-dom";
 import Comments from "../components/comments/Comments";
-import HighlightedQuote from '../components/quotes/HighlightedQuote'
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
-const DUMMY_QUOTES=[{
-  id:1,author:'hema',text:'new quotes-1'
-},{
-  id:2,author:'hema1',text:'new quotes-2'
-},{
-  id:3,author:'hema2',text:'new quotes-3'
-}]
+const DUMMY_QUOTES = [
+  {
+    id: 1,
+    author: "hema",
+    text: "new quotes-1",
+  },
+  {
+    id: 2,
+    author: "hema1",
+    text: "new quotes-2",
+  },
+  {
+    id: 3,
+    author: "hema2",
+    text: "new quotes-3",
+  },
+];
 
 const QuoteDetails = () => {
   const params = useParams();
-  let quoteId=Number(params.id);  
-  const quote=DUMMY_QUOTES.find((quote)=> quote.id === quoteId);
+  let quoteId = Number(params.id);
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === quoteId);
 
-  if(!quote)
-   return <p>No Quote found</p>
+  if (!quote) return <p>No Quote found</p>;
 
   return (
     <div>
-     <HighlightedQuote author={quote.author} text={quote.text} />
+      <HighlightedQuote author={quote.author} text={quote.text} />
+      <Link to='comments'>Load Comments</Link>
       <Routes>
         <Route path='comments' element={<Comments />} />
       </Routes>
