8ba6323854f9b408c99e0713df7faa06dd1a4738
hema ganesan <hema@skiploop.com>
2021-12-22 18:07:59 +0530

    navigation to page

--- src/pages/NewQuotes.js
+++ src/pages/NewQuotes.js
@@ -1,9 +1,13 @@
+import {useNavigate} from 'react-router-dom'
 import QuoteForm from '../components/quotes/QuoteForm'
 
 const NewQuotes = () => {
+  const navigate = useNavigate();
 
  const onAddQuoteHandler=(quotedData)=>{
-   console.log(quotedData)
+   console.log(quotedData);
+   navigate('/quotes');
+
  }
   return <QuoteForm onAddQuote={onAddQuoteHandler}/>
 };
